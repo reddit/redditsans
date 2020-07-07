2b3c5e0ff155956d8368aae5b1af51aff6b3ffe1
Stephen Hutchings <stephen@creativelicence.com.au>
2020-07-07 20:07:40 +1000

    Prepare for added versions beyond v1.000

--- specimen/src/11ty/_data/font.js
+++ specimen/src/11ty/_data/font.js
@@ -98,9 +98,20 @@ const features = [
 ]
 
 const archive =
-  Array.from({length: 12}, (e, i) =>
-    "0.0" + (i < 9 ? "0" : "") + (i + 1)
-  ).reverse()
+[
+  "0.012",
+  "0.011",
+  "0.010",
+  "0.009",
+  "0.008",
+  "0.007",
+  "0.006",
+  "0.005",
+  "0.004",
+  "0.003",
+  "0.002",
+  "0.001",
+]
 
 function getData() {
   const content = fs.readFileSync("../src/Reddit Sans.glyphs").toString()

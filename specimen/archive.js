68b14cc665b28c4139dfb6672806c605ca2e3b70
Stephen Hutchings <stephen@creativelicence.com.au>
2021-08-15 23:44:09 +1000

    Update archive.js

--- specimen/archive.js
+++ specimen/archive.js
@@ -1,20 +1,21 @@
 const fs = require("fs")
 const archiver = require("archiver")
-const font = require("./src/site/_data/font")
 
-const dist = `./dist/Reddit Sans ${font.version}.zip`
-const output = fs.createWriteStream(dist)
-const archive = archiver("zip", { zlib: { level: 9 } })
+require("./src/site/_data/font").then(({ version }) => {
+  const dist = `./dist/Reddit Sans ${version}.zip`
+  const output = fs.createWriteStream(dist)
+  const archive = archiver("zip", { zlib: { level: 9 } })
 
-output.on("close", function () {
-  console.log(`Archived fonts to ${dist}`)
-  console.log((archive.pointer() / 1000 / 1000).toFixed(1) + "MB")
+  output.on("close", function () {
+    console.log(`Archived fonts to ${dist}`)
+    console.log((archive.pointer() / 1000 / 1000).toFixed(1) + "MB")
+  })
+
+  archive.pipe(output)
+
+  archive.directory("./dist/fonts/desktop", "fonts/desktop")
+  archive.directory("./dist/fonts/web", "fonts/web")
+  archive.directory("../documents", false)
+
+  archive.finalize()
 })
-
-archive.pipe(output)
-
-archive.directory("./dist/fonts/desktop", "fonts/desktop")
-archive.directory("./dist/fonts/web", "fonts/web")
-archive.directory("../documents", false)
-
-archive.finalize()

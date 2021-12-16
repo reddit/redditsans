0a408ab35b1e25d55ed211a053141d23ba71b9dd
Stephen Hutchings <stephen@creativelicence.com.au>
2021-12-16 14:50:29 +1100

    Store and retrieve style state via URL params

--- specimen/src/assets/js/components/style.js
+++ specimen/src/assets/js/components/style.js
@@ -1,17 +1,52 @@
 const select = require("../utilities/select")
 
+const encode = (style) => {
+  return Array.from(style).reduce(
+    (m, key, i) =>
+      m +
+      (i == 0 ? "?" : "&") +
+      encodeURIComponent(key) +
+      "=" +
+      encodeURIComponent(style[key]),
+    ""
+  )
+}
+
+const decode = (params) => {
+  return Object.fromEntries(
+    params
+      .slice(1)
+      .split(/&/)
+      .map((group) => group.split("=").map(decodeURIComponent))
+      .filter(([key, val]) => val)
+  )
+}
+
 const update = (node) => (e) => {
   select()(e)
 
   Object.keys(e.currentTarget.dataset).forEach((k) => {
     node.style[k] = e.currentTarget.dataset[k]
   })
+
+  window.history.replaceState(
+    {},
+    document.title,
+    window.location.pathname + encode(node.style)
+  )
 }
 
 module.exports = {
   initialize: (node) => {
-    Array.from(node.querySelectorAll("button")).forEach((el) => {
+    const style = decode(window.location.search)
+    const buttons = Array.from(node.querySelectorAll("button"))
+
+    buttons.forEach((el) => {
       el.addEventListener("click", update(node))
+    })
+
+    Object.entries(style).forEach(([key, val]) => {
+      node.querySelector(`[data-${key}="${val}"]`).click()
     })
   },
 }

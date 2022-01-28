deba9b9986e93ed31fa09a220bf51fe855f9056b
Stephen Hutchings <stephen@creativelicence.com.au>
2022-01-28 17:33:38 +1100

    Update font.js

--- specimen/src/site/_data/font.js
+++ specimen/src/site/_data/font.js
@@ -77,7 +77,7 @@ async function getData() {
       codes: glyphs.filter((e) => e.codes).length,
       total: glyphs.length,
     },
-    monospacedVersions: ["01"],
+    monospacedVersions: ["02", "01"],
     condensedVersions: ["04", "03", "02", "01"],
   }
 }
